974338ea903b5ba24bc25fdea40e3773bf7e2503
Bradley Thompson <31293826+Bisrad@users.noreply.github.com>
2020-09-21 14:58:24 -0700

    Add basic setup / procedurally generated calendar

--- js/app.js
+++ js/app.js
@@ -3,3 +3,18 @@
     $(document).ready(function(){
        console.log("document ready");
     });
+
+// Procedurally generate 30 day object
+
+var cont = document.getElementById("calender");
+
+for(var i = 1; i <31; ++i){
+ var div = document.createElement("div");
+ div.setAttribute("class", "square");
+ div.setAttribute("id", "div" + i);
+ 
+ var h1 = document.createElement("h1");
+   h1.textContent = i;
+   div.appendChild(h1);
+   cont.appendChild(div);
+ }
